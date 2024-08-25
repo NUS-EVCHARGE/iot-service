5ef436226866a39eb97050e79a69c7ed86793705
gitkh2891 <123794724+gitkh2891@users.noreply.github.com>
2024-08-25 18:48:03 +0800

    Updated mvp by extracting code from provider service
    Included the GetServiceHealthCheck function that Terry pushed into the mvp IOT_service.go file. Uploaded the go.mod and go.sum after running the "go mod tidy" command

--- go.mod
+++ go.mod
@@ -1,9 +1,64 @@
-module github.com/NUS-EVCHARGE/iot-service
+module iot-service/iot_service
 
-go 1.21.5
+go 1.22.5
 
 require (
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/NUS-EVCHARGE/ev-provider-service v0.0.0-20240810100634-a540ee8ccc84
+	github.com/gin-contrib/cors v1.4.0
+	github.com/gin-gonic/gin v1.10.0
+	github.com/gorilla/websocket v1.5.3
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/aws/aws-sdk-go v1.53.20 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.24.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.26.4 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.16.15 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.26.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.18.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.26.7 // indirect
+	github.com/aws/smithy-go v1.19.0 // indirect
+	github.com/bytedance/sonic v1.11.6 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.20.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/arch v0.8.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+	googlemaps.github.io/maps v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.1 // indirect
+	gorm.io/gorm v1.25.2 // indirect
 )
--- iot_service.go
+++ iot_service.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/sirupsen/logrus"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	"github.com/NUS-EVCHARGE/iot-service/handler"
+)
+
+var (
+	r *gin.Engine
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
@@ -47,135 +53,48 @@ const (
 	Error
 )
 
-// get charger status
-func getChargerStatus(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "application/json")
-
-	switch r.Method {
-	case "GET":
-		chargerId := r.URL.Query().Get("charger_id")
-		if charger, ok := chargerIdMap[chargerId]; ok {
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(charger.status)
-			return
-		}
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode("charger not found")
-	case "POST":
-		logrus.Info("setting_charger_status")
-		var bodyBytes []byte
-		var err error
-		var req IoTHubRequest
-		mapLock.Lock()
-		defer mapLock.Unlock()
-
-		if r.Body != nil {
-			bodyBytes, err = ioutil.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("Body reading error: %v", err)
-				return
-			}
-			defer r.Body.Close()
-		}
-
-		if err = json.Unmarshal(bodyBytes, &req); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode("payload unmarshall failed")
-		}
-
-		if charger, ok := chargerIdMap[req.ChargerId]; ok {
-			charger.status = req.Status
-			chargerIdMap[req.ChargerId] = charger
-			w.WriteHeader(http.StatusOK)
-			// requires to add charger status validation
-			json.NewEncoder(w).Encode(charger.status)
-			charger.conn.WriteMessage(charger.messageType, []byte(fmt.Sprintf("%v", charger.status)))
-			return
-		}
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode("charger not found")
-	}
-}
-
-func getServiceHealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode("service is up and running")
-}
-
-// communication with charging point
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-
-	for {
-		mt, message, err := c.ReadMessage()
-		// business logic here
-
-		if err != nil {
-			logrus.WithField("err", err).Error("failed_to_read_message_from_client")
-			break
-		}
-
-		// parse message as json
-		var req IoTHubRequest
-		err = json.Unmarshal(message, &req)
-		if err != nil {
-			logrus.WithField("req", string(message)).WithField("err", err).Error("failed_to_unmarshall_client_request")
-		}
-		mapLock.Lock()
-		defer mapLock.Unlock()
-		switch req.Command {
-		case "register":
-			chargerIdMap[req.ChargerId] = Instance{
-				conn:        c,
-				ChargerId:   req.ChargerId,
-				status:      Pending,
-				messageType: mt,
-			}
-			if err := c.WriteMessage(mt, []byte("instance register success")); err != nil {
-				logrus.WithField("err", err).Error("failed_to_write_message_to_client")
-			}
-		case "unregister":
-			if _, ok := chargerIdMap[req.ChargerId]; ok {
-				delete(chargerIdMap, req.ChargerId)
-				if err := c.WriteMessage(mt, []byte("instance unregister success")); err != nil {
-					logrus.WithField("err", err).Error("failed_to_write_message_to_client")
-				}
-			} else {
-				logrus.WithField("req", req).Error("instance_does_not_exist")
-			}
-		default:
-			// status switching
-			if instance, ok := chargerIdMap[req.ChargerId]; ok {
-				instance.status = req.Status
-				chargerIdMap[req.ChargerId] = instance
-				if err := c.WriteMessage(mt, []byte("instance status update success")); err != nil {
-					logrus.WithField("err", err).Error("failed_to_write_message_to_client")
-				}
-			} else {
-				err := c.WriteMessage(mt, []byte("instance not registered"))
-				logrus.WithField("err", err).Error("failed_to_write_message_to_client")
-			}
-
-		}
-		mapLock.Unlock()
-	}
-}
-
 func main() {
-	flag.Parse()
-	http.HandleFunc("/ws", wsEndpoint)
-	http.HandleFunc("/charger", getChargerStatus)
-	http.HandleFunc("/iot/health", getServiceHealthCheck)
 
-	logrus.Info("serving_iot_service...")
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	// self-signed cert and key in same folder to support https and wss
+	tlsCert := "mkcert.pem"
+	tlsKey := "mkkey.pem"
+
+	flag.Parse()
+
+	r = gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*", "https://localhost:443"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS", "HEAD"},
+		AllowHeaders:     []string{"authorization", "access-control-allow-origin", "Access-Control-Allow-Headers", "Origin", "Content-Length", "Content-Type", "authentication", "Access-Control-Request-Method", "Access-Control-Request-Headers"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	registerHandler()
+
+	// amended r.Run to r.RunTLS with additional arguments to implement https and wss
+	if err := r.RunTLS(":443", tlsCert, tlsKey); err != nil {
+		logrus.WithField("error", err).Errorf("http server failed to start")
 	}
+
+}
+
+func registerHandler() {
+	// use to generate swagger ui
+	//	@BasePath	/api/v1
+	//	@title		IOT Service API
+	//	@version	1.0
+	//	@schemes	http
+
+	// api versioning
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/ws", handler.WsChargerEndpoint)
+		v1.GET("/charger/chargerstatus", handler.GetChargerEndpointStatus)
+		v1.POST("/charger/chargerstatus", handler.SetChargerEndpointStatus)
+
+		// additional API route for Terry's implementation of service health check
+		v1.GET("/iot/health", handler.GetServiceHealthCheck)
+	}
+
 }
